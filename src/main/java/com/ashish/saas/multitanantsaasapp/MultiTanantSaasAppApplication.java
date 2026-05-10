d57c4a64d244f04fb9b33dbbad476f0b3e9ad12d
Ashish jha <ajha5645@gmail.com>
2026-05-10 23:54:59 +0530

    Initial commit

---
+++ src/main/java/com/ashish/saas/multitanantsaasapp/MultiTanantSaasAppApplication.java
@@ -0,0 +1,13 @@
+package com.ashish.saas.multitanantsaasapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiTanantSaasAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiTanantSaasAppApplication.class, args);
+    }
+
+}
