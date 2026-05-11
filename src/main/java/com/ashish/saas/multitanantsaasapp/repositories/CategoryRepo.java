31b1752450c683866a7d7f896ae9092590a0fc49
Ashish jha <ajha5645@gmail.com>
2026-05-11 20:09:20 +0530

    Add ApiError and AppException classes for improved error handling; create CategoryController

--- src/main/java/com/ashish/saas/multitanantsaasapp/repositories/CategoryRepo.java
+++ src/main/java/com/ashish/saas/multitanantsaasapp/repositories/CategoryRepo.java
@@ -4,6 +4,9 @@ import com.ashish.saas.multitanantsaasapp.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CategoryRepo extends  JpaRepository<Category, String>{
+    Optional<Category> findByNameIgnoreCase(String name);
 }
--- src/main/java/com/ashish/saas/multitanantsaasapp/services/CategoryService.java
+++ src/main/java/com/ashish/saas/multitanantsaasapp/services/CategoryService.java
@@ -1,7 +1,8 @@
 package com.ashish.saas.multitanantsaasapp.services;
 
 import com.ashish.saas.multitanantsaasapp.dto.request.CategoryRequest;
+import com.ashish.saas.multitanantsaasapp.dto.response.CategoryResponse;
 
-public interface CategoryService extends BasicService<CategoryRequest,CategoryResponse>{
+public interface CategoryService extends BasicService<CategoryRequest, CategoryResponse>{
     void create(CategoryRequest request);
 }
