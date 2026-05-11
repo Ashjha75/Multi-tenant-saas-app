467859674f5b32ef1c23493d9a4f40b6f6586825
Ashish jha <ajha5645@gmail.com>
2026-05-11 17:20:05 +0530

    Add CategoryMapper and update CategoryService and CategoryServiceImpl to include create method

--- src/main/java/com/ashish/saas/multitanantsaasapp/mapper/CategoryMapper.java
+++ src/main/java/com/ashish/saas/multitanantsaasapp/mapper/CategoryMapper.java
@@ -1,4 +1,29 @@
 package com.ashish.saas.multitanantsaasapp.mapper;
 
+import com.ashish.saas.multitanantsaasapp.dto.request.CategoryRequest;
+import com.ashish.saas.multitanantsaasapp.dto.response.CategoryResponse;
+import com.ashish.saas.multitanantsaasapp.entities.Category;
+import org.springframework.stereotype.Service;
+
+import static com.ashish.saas.multitanantsaasapp.dto.response.CategoryResponse.*;
+
+@Service
 public class CategoryMapper {
+
+    public Category toEntity(final CategoryRequest request){
+        return Category.builder().
+                name(request.getName()).
+                description(request.getDescription()).
+                deleted(false).
+                build();
+    }
+
+    public CategoryResponse toResponse(final Category entity){
+        return builder().
+                id(entity.getId()).
+                name(entity.getName()).
+                description(entity.getDescription())
+                .build();
+    }
+
 }
