4fde6d1449378a809a994c0edb5dadd2f7e15870
Ashish jha <ajha5645@gmail.com>
2026-05-11 16:54:08 +0530

    Add CategoryRequest, CategoryResponse, and CategoryService classes

--- src/main/java/com/ashish/saas/multitanantsaasapp/dto/request/CategoryRequest.java
+++ src/main/java/com/ashish/saas/multitanantsaasapp/dto/request/CategoryRequest.java
@@ -1,4 +1,13 @@
 package com.ashish.saas.multitanantsaasapp.dto.request;
 
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CategoryRequest {
+    private String name;
+    private String description;
 }
--- src/main/java/com/ashish/saas/multitanantsaasapp/dto/response/CategoryResponse.java
+++ src/main/java/com/ashish/saas/multitanantsaasapp/dto/response/CategoryResponse.java
@@ -1,4 +1,14 @@
 package com.ashish.saas.multitanantsaasapp.dto.response;
 
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CategoryResponse {
+    private String name;
+    private String description;
+
 }
