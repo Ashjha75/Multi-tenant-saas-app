3e315d3171837a005513dc30e5114bbae3e74440
Ashish jha <ajha5645@gmail.com>
2026-05-11 14:49:01 +0530

    Implement Category entity with auditing support and update application configuration

--- src/main/java/com/ashish/saas/multitanantsaasapp/MultiTanantSaasAppApplication.java
+++ src/main/java/com/ashish/saas/multitanantsaasapp/MultiTanantSaasAppApplication.java
@@ -2,8 +2,10 @@ package com.ashish.saas.multitanantsaasapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MultiTanantSaasAppApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/ashish/saas/multitanantsaasapp/entities/Category.java
+++ src/main/java/com/ashish/saas/multitanantsaasapp/entities/Category.java
@@ -1,4 +1,26 @@
 package com.ashish.saas.multitanantsaasapp.entities;
+import com.ashish.saas.multitanantsaasapp.common.AbstractEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.domain.Auditable;
 
-public class Category {
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="categories")
+public class Category extends AbstractEntity {
+    @Column(name = "name", nullable = false)
+    private String name;
+    
+    @Column(name = "description")
+    private String description;
 }
