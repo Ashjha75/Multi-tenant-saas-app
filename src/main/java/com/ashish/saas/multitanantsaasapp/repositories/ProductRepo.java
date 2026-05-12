357bb2164f5e6e94ca2dd7daff451f9de16ba2ae
Ashish jha <ajha5645@gmail.com>
2026-05-12 20:39:15 +0530

    feat: add StockMvt repository and annotate ProductRepo with @Repository

--- src/main/java/com/ashish/saas/multitanantsaasapp/repositories/ProductRepo.java
+++ src/main/java/com/ashish/saas/multitanantsaasapp/repositories/ProductRepo.java
@@ -2,9 +2,10 @@ package com.ashish.saas.multitanantsaasapp.repositories;
 
 import com.ashish.saas.multitanantsaasapp.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface ProductRepo extends JpaRepository<Product,String> {
     Optional<Product> findByReferenceIgnoreCase(String reference);
 }
---
+++ src/main/java/com/ashish/saas/multitanantsaasapp/repositories/StockMvtRepo.java
@@ -0,0 +1,9 @@
+package com.ashish.saas.multitanantsaasapp.repositories;
+
+import com.ashish.saas.multitanantsaasapp.entities.StockMvt;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StockMvtRepo extends JpaRepository<StockMvt,String> {
+}
