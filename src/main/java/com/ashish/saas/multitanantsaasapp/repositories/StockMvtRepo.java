ad31a246766ac5ef3832a20f90659d1f433f6a98
Ashish jha <ajha5645@gmail.com>
2026-05-28 11:13:47 +0530

    Format repository interface generics

--- src/main/java/com/ashish/saas/multitanantsaasapp/repositories/StockMvtRepo.java
+++ src/main/java/com/ashish/saas/multitanantsaasapp/repositories/StockMvtRepo.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StockMvtRepo extends JpaRepository<StockMvt,String> {
+public interface StockMvtRepo extends JpaRepository<StockMvt, String> {
 }
--- src/main/java/com/ashish/saas/multitanantsaasapp/repositories/TenantRepo.java
+++ src/main/java/com/ashish/saas/multitanantsaasapp/repositories/TenantRepo.java
@@ -8,8 +8,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TenantRepo extends JpaRepository<Tenant,String> {
-    boolean existsByCompanyCode (String companyCode);
-    boolean existsByEmail(String email) ;
+public interface TenantRepo extends JpaRepository<Tenant, String> {
+    boolean existsByCompanyCode(String companyCode);
+
+    boolean existsByEmail(String email);
+
     Page<Tenant> findAllByStatus(TenantStatus status, Pageable pageable);
 }
